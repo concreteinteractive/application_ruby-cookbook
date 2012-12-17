8cb609811a83baacdb49ad590c265a2d67d3c2e7
Pranas Kiziela <pranas.kiziela@gmail.com>
2012-12-17 15:03:38 -0800

    Add nginx frontend resource for Unicorns.

--- metadata.rb
+++ metadata.rb
@@ -6,6 +6,6 @@ description      "Deploys and configures Ruby-based applications"
 long_description  IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          "1.0.6"
 
-%w{ application runit unicorn apache2 passenger_apache2 }.each do |cb|
+%w{ application runit unicorn apache2 passenger_apache2 nginx}.each do |cb|
   depends cb
 end
